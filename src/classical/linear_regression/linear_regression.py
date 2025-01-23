829f732f264f71d933d7bae741ed8044c7226e61
Cole Foster <cmf290@miami.edu>
2025-01-23 09:28:03 -0500

    Modified input validation

--- src/classical/linear_regression/linear_regression.py
+++ src/classical/linear_regression/linear_regression.py
@@ -5,25 +5,40 @@ class LinearRegression(ClassicalModel):
     """
     A simple implementation of Linear Regression using Ordinary Least Squares (OLS)
     """
-    def __init__(self):
-        """Initialize the model parameters"""
-        self.params = None
     
-    def _validate_input(self, array: np.ndarray) -> None:
+    def __init__(self) -> None:
+        """Initialize the model parameters"""
+        self.params = {'beta': None}
+    
+    def _validate_input(self, array1: np.ndarray, array2: np.ndarray = None) -> None:
         """
-        Validate the input numpy array
+        Validate the input numpy arrays
 
         Args:
             array (np.ndarray): Input numpy array
         """
-        if not isinstance(array, np.ndarray):
-            raise TypeError("Input must be a numpy array")
+        if not isinstance(array1, np.ndarray):
+            raise TypeError("Array 1 must be a numpy array")
         
-        if len(array.shape) != 2:
-            raise ValueError("Input must be a 2D array")
+        if len(array1.shape) != 2:
+            raise ValueError("Array 1 must be a 2D array")
         
-        if array.size == 0:
-            raise ValueError("Input must not be empty")
+        if array1.size == 0:
+            raise ValueError("Array 1 must not be empty")
+        
+        if array2 is not None:
+            if not isinstance(array2, np.ndarray):
+                raise TypeError("Array 2 must be a numpy array")
+        
+            if len(array2.shape) != 2:
+                raise ValueError("Array 2 must be a 2D array")
+            
+            if array2.size == 0:
+                raise ValueError("Array 2 must not be empty")
+            
+            # Check if the number of rows in X and Y are equal
+            if array1.shape[0] != array2.shape[0]:
+                raise ValueError("The number of rows in X and Y must be equal")
 
     def fit(self, X: np.ndarray, Y: np.ndarray) -> None:
         """
@@ -34,15 +49,10 @@ class LinearRegression(ClassicalModel):
             Y (np.ndarray): Target matrix of shape (n_samples, 1)
         """
         # Validate the input arrays
-        self._validate_input(X)
-        self._validate_input(Y)
-        
-        # Check if the number of rows in X and Y are equal
-        if X.shape[0] != Y.shape[0]:
-            raise ValueError("The number of rows in X and Y must be equal")
+        self._validate_input(X,Y)
         
         # Evaluate the model parameters using OLS
-        self.params = {'beta': np.linalg.pinv(X.T @ X) @ X.T @ Y}
+        self.params['beta'] = np.linalg.pinv(X.T @ X) @ X.T @ Y
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -54,8 +64,10 @@ class LinearRegression(ClassicalModel):
         Returns:
             np.ndarray: Predicted values of shape (n_samples, n_targets)
         """
-        if self.params is None:
+        if not self.params:
             raise ValueError("Fit the model before making predictions")
+        
+        self._validate_input(X)
         
         if X.shape[1] != self.params['beta'].shape[0]:
             raise ValueError("The number of columns in X must be equal to the number of features in the model")
@@ -73,8 +85,7 @@ class LinearRegression(ClassicalModel):
         Returns:
             float: Mean Squared Error
         """
-        self._validate_input(Y_pred)
-        self._validate_input(Y)
+        self._validate_input(Y_pred, Y)
         SCALE_FACTOR = 0.5 / Y.shape[0]
         return SCALE_FACTOR * np.linalg.norm(Y_pred - Y)**2
 
@@ -86,5 +97,3 @@ class LinearRegression(ClassicalModel):
             dict: Model parameters {'beta': np.ndarray}
         """
         return self.params
-
-    
