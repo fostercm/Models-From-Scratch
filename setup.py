e21fd10861de70728d542435de83b80c84dc97f5
Cole Foster <cmf290@miami.edu>
2025-01-22 19:02:49 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MLFromScratch',
+    version='v0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "numpy"
+    ]
+)
