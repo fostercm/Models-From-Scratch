3dba2278ad6507fbfd5190ab59fbd99c457dbec2
Cole Foster <cmf290@miami.edu>
2025-01-26 19:44:28 -0500

    psuedoinverse commenting

--- src/utils/c_utils/pseudoinverse.h
+++ src/utils/c_utils/pseudoinverse.h
@@ -2,7 +2,7 @@
 #define PSEUDOINVERSE_H
 
 // Function declaration
-void compute_pseudoinverse(double *A, double *A_inv, int m, int n);
+void computePseudoinverse(double *A, double *A_inv, int m, int n);
 void print_matrix(double *A, int m, int n);
 
 #endif // PSEUDOINVERSE_H
