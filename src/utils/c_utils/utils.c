e11c5fe47d92a361bc87dd36f7d5e9b6320e90c8
Cole Foster <cmf290@miami.edu>
2025-01-26 19:43:58 -0500

    help functions split to utils

---
+++ src/utils/c_utils/utils.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Function to print a matrix
+void printMatrix(double *A, int m, int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f ", A[i * n + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
---
+++ src/utils/c_utils/utils.h
@@ -0,0 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void printMatrix(double *A, int m, int n);
+
+#endif /* UTILS_H */
