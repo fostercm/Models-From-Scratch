570c945eca8b7543c5d9633703a8f171a9a0c0ca
Cole Foster <cmf290@miami.edu>
2025-01-28 22:48:28 -0500

    Comment update

--- src/utils/c_utils/pseudoinverse_c.h
+++ src/utils/c_utils/pseudoinverse_c.h
@@ -1,7 +1,6 @@
 #ifndef PSEUDOINVERSE_C_H
 #define PSEUDOINVERSE_C_H
 
-// Function declaration
 void computePseudoinverse(float *A, float *A_inv, const int m, const int n);
 
 #endif // PSEUDOINVERSE_C_H
