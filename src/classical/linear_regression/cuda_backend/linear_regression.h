ff4477641718f1077f37f15e8324111930f5f20e
Cole Foster <cmf290@miami.edu>
2025-01-29 22:13:58 -0500

    Removed old functions

--- src/classical/linear_regression/cuda_backend/linear_regression.h
+++ src/classical/linear_regression/cuda_backend/linear_regression.h
@@ -9,8 +9,6 @@ void fit(const float *X, const float *Y, float *Beta, const int num_samples, con
 void predict(const float *X, const float *Beta, float *Prediction, const int num_samples, const int num_input_features, const int num_output_features);
 float cost(const float *Y_pred, const float *Y, const int num_samples, const int num_output_features);
 
-__global__ void vectorDifferenceKernel(const float *a, const float *b, float *result, const int n);
-void launchVectorDifference(const float *a, const float *b, float *result, const int n);
 void fitCUDA(const float *X, const float *Y, float *Beta, const int num_samples, const int num_input_features, const int num_output_features);
 void predictCUDA(const float *X, const float *Beta, float *Prediction, const int num_samples, const int num_input_features, const int num_output_features);
 void costCUDA(const float *Y_pred, const float *Y, float *cost, const int num_samples, const int num_output_features);
