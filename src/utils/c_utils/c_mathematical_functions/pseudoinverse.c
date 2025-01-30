22d122f7e3d9a07cc09b9189e3c6c63cb93ece6a
Cole Foster <cmf290@miami.edu>
2025-01-29 20:14:18 -0500

    Added safe memory to pseudoinverse

--- src/utils/c_utils/c_mathematical_functions/pseudoinverse.c
+++ src/utils/c_utils/c_mathematical_functions/pseudoinverse.c
@@ -1,7 +1,9 @@
 #include "pseudoinverse.h"
+#include "../c_memory_functions/memory_functions.h"
 #include <lapacke.h>
 #include <cblas.h>
 #include <stdio.h>
+#include <string.h>
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 
@@ -9,52 +11,56 @@
 void computePseudoinverse(float *A, float *A_inv, const int m, const int n) {
 
     // Allocate memory for the SVD results
-    float *U = (float *) malloc(m * m * sizeof(float));
-    float *S = (float *) malloc(MIN(m, n) * sizeof(float));
-    float *V_transpose = (float *) malloc(n * n * sizeof(float));
-    float *superb = (float *) malloc(MIN(m, n) * sizeof(float));
-
-    // Allocate memory for matrix multiplication intermediates
-    float *S_inverse = (float *) calloc(m * n, sizeof(float));
-    float *VS_inverse = (float *) malloc(n * m * sizeof(float));
-    
-    // Check if memory allocation failed
-    if (U == NULL || S == NULL || V_transpose == NULL || superb == NULL || S_inverse == NULL || VS_inverse == NULL) {
-        fprintf(stderr, "Memory allocation failed.\n");
-        exit(EXIT_FAILURE);
-    }
+    float *U, *S, *V_T, *superb;
+    U = (float*) safeMalloc(m * m * sizeof(float));
+    S = (float*) safeMalloc(MIN(m, n) * sizeof(float));
+    V_T = (float*) safeMalloc(n * n * sizeof(float));
+    superb = (float*) safeMalloc(MIN(m, n) * sizeof(float));
 
     // Compute the SVD and free superb
-    int info = LAPACKE_sgesvd(LAPACK_ROW_MAJOR, 'A', 'A', m, n, A, n, S, U, m, V_transpose, n, superb);
-    free(superb);
-
-    // Check if the SVD was successful
-    if (info > 0) {
-        fprintf(stderr, "The algorithm computing SVD failed to converge.\n");
-        exit(EXIT_FAILURE);
-    }
+    LAPACKE_sgesvd(LAPACK_ROW_MAJOR, 'A', 'A', m, n, A, n, S, U, m, V_T, n, superb);
+    safeFree(superb);
 
     // Diagonalize and invert non-zero singular values (create S+)
+    float *S_inv;
+    S_inv = (float*) safeMalloc(m * n * sizeof(float));
+    memset(S_inv, 0, m * n * sizeof(float));
     for (int i = 0; i < MIN(m, n); i++) {
         if (S[i] > 1e-15) {
-            S_inverse[i * m + i] = 1.0 / S[i];
+            S_inv[i * m + i] = 1.0 / S[i];
         }
     }
 
     // Free S
-    free(S);
+    safeFree(S);
 
-    // Multiply V (V_transpose_transpose) and S+ to get VS_inverse
-    cblas_sgemm(CblasRowMajor, CblasTrans, CblasNoTrans, n, m, n, 1, V_transpose, n, S_inverse, m, 0, VS_inverse, m);
+    // Compute VS_inv = V * S_inv
+    float *VS_inv;
+    VS_inv = (float *) malloc(n * m * sizeof(float));
+    cblas_sgemm(
+        CblasRowMajor, CblasTrans, CblasNoTrans, 
+        n, m, n, 
+        1, 
+        V_T, n, 
+        S_inv, m, 
+        0, 
+        VS_inv, m);
 
     // Free VT and S_Inverse
-    free(V_transpose);
-    free(S_inverse);
+    safeFree(V_T);
+    safeFree(S_inv);
 
-    // Multiply VS_inverse and U_transpose to get the pseudoinverse
-    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasTrans, n, m, m, 1, VS_inverse, m, U, m, 0, A_inv, m);
+    // Compute A_inv = VS_inv * U (V * S+ * U)
+    cblas_sgemm(
+        CblasRowMajor, CblasNoTrans, CblasTrans, 
+        n, m, m, 
+        1, 
+        VS_inv, m, 
+        U, m, 
+        0, 
+        A_inv, m);
 
     // Free U and VSI
-    free(U);
-    free(VS_inverse);
+    safeFree(U);
+    safeFree(VS_inv);
 }
