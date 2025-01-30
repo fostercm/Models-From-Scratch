6e3c9155ea6da9d5d20578c6c45f6c7132121fa4
Cole Foster <cmf290@miami.edu>
2025-01-29 19:26:01 -0500

    Added print matrix

---
+++ src/utils/c_utils/matrix_functions/matrix_functions.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "matrix_functions.h"
+
+// Function to print a matrix
+void printMatrix(const float *A, const int m, const int n) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%f ", A[i * n + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
--- src/utils/cuda_utils/matrix_functions/matrix_functions.h
+++ src/utils/cuda_utils/matrix_functions/matrix_functions.h
@@ -1,6 +1,7 @@
 #ifndef MATRIX_FUNCTIONS_H
 #define MATRIX_FUNCTIONS_H
 
+void printMatrixCUDA(const float *matrix, const int rows, const int cols);
 __global__ void populateDiagonalKernel(float *matrix, const float *diagonal, const int m, const int n);
 void launchPopulateDiagonalKernel(float *matrix, const float *diagonal, const int m, const int n);
 
