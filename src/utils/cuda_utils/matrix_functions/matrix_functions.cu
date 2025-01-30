729a950f8c01051e373e462bf28a62618424c854
Cole Foster <cmf290@miami.edu>
2025-01-29 19:17:47 -0500

    Added matrix display for debugging

--- src/utils/cuda_utils/matrix_functions/matrix_functions.cu
+++ src/utils/cuda_utils/matrix_functions/matrix_functions.cu
@@ -1,6 +1,23 @@
 #include <cuda_runtime.h>
+#include <iostream>
 
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+void printMatrixCUDA(const float *matrix, const int rows, const int cols) {
+
+    // Copy matrix from device to host
+    float *h_matrix = (float *) malloc(rows * cols * sizeof(float));
+    cudaMemcpy(h_matrix, matrix, rows * cols * sizeof(float), cudaMemcpyDeviceToHost);
+
+    // Print matrix
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%f ", h_matrix[i * cols + j]);
+        }
+        printf("\n");
+    }
+    free(h_matrix);
+}
 
 __global__ void populateDiagonalKernel(float *matrix, const float *diagonal, const int m, const int n) {
     // Get thread ID
