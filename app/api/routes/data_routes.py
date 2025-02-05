54227501467b1127b3320ce43f1f3bd82962c741
Cole Foster <cmf290@miami.edu>
2025-02-05 14:12:07 -0500

    App refactoring

--- app/api/routes/data_routes.py
+++ app/api/routes/data_routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
-from api.schemas.data_schemas import DataUploadRequest
-from api.services.data_services import store_data
+from schemas.data_schemas import DataUploadRequest
+from services.data_services import store_data
 
 router = APIRouter()
 
