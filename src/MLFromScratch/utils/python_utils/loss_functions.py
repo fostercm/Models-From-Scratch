bb45b570a023bcd91f0792d2cc7004cbbd7ecf39
Cole Foster <cmf290@miami.edu>
2025-02-12 17:56:09 -0500

    Added python cross entropy loss

--- src/MLFromScratch/utils/python_utils/loss_functions.py
+++ src/MLFromScratch/utils/python_utils/loss_functions.py
@@ -18,3 +18,22 @@ def meanSquaredError(Y_pred: np.ndarray, Y_true: np.ndarray) -> float:
     """
     SCALE_FACTOR = 0.5 / Y_true.shape[0]
     return SCALE_FACTOR * np.linalg.norm(Y_pred - Y_true)**2
+
+def crossEntropy(Y_pred: np.ndarray, Y_true: np.ndarray) -> float:
+    """
+    Compute the cross-entropy loss between the predicted and true labels.
+    
+    Parameters
+    ----------
+    Y_pred : numpy array
+        Predicted labels.
+    Y_true : numpy array
+        True labels.
+    
+    Returns
+    -------
+    float
+        Cross-entropy loss.
+    """
+    SCALE_FACTOR = -1 / Y_true.shape[0]
+    return SCALE_FACTOR * np.sum(Y_true * np.log(Y_pred + 1e-9))
