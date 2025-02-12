2f4f47280f5bfab6a5d4ce5ee1cff255cc617e7f
Cole Foster <cmf290@miami.edu>
2025-02-12 16:20:24 -0500

    Added activation functions for python

---
+++ src/MLFromScratch/utils/python_utils/activation_functions.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def sigmoid(X: np.ndarray) -> np.ndarray:
+    """
+    Compute the sigmoid function for the input array X.
+    
+    Parameters
+    ----------
+    X : numpy array
+        Input array.
+    
+    Returns
+    -------
+    numpy array
+        Sigmoid of the input array.
+    """
+    return 1 / (1 + np.exp(-X))
+
+def softmax(X: np.ndarray) -> np.ndarray:
+    """
+    Compute the softmax function for the input array X.
+    
+    Parameters
+    ----------
+    X : numpy array
+        Input array.
+    
+    Returns
+    -------
+    numpy array
+        Softmax of the input array.
+    """
+    exps = np.exp(X - np.max(X, axis=1, keepdims=True))
+    return exps / np.sum(exps, axis=1, keepdims=True)
