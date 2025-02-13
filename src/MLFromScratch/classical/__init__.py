4ede3a61844f3ef5ee7ff889e583a1f4b7b47253
Cole Foster <cmf290@miami.edu>
2025-02-13 13:08:29 -0500

    Added logistic regression to import

--- src/MLFromScratch/classical/__init__.py
+++ src/MLFromScratch/classical/__init__.py
@@ -2,4 +2,5 @@ from .linear_regression.py_interface.linear_regression_python import LinearRegre
 from .linear_regression.py_interface.linear_regression_c import LinearRegressionC
 from .linear_regression.py_interface.linear_regression_cuda import LinearRegressionCUDA
 from .logistic_regression.py_interface.logistic_regression_python import LogisticRegressionPython
+from .logistic_regression.py_interface.logistic_regression_c import LogisticRegressionC
 from .classical_model import ClassicalModel
