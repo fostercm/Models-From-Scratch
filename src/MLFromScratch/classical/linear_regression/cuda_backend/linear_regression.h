0ac2e7ce176335ef211e7da11ca0d1471736542d
Cole Foster <cmf290@miami.edu>
2025-02-16 23:58:54 -0500

    Syntax fix

--- src/MLFromScratch/classical/linear_regression/cuda_backend/linear_regression.h
+++ src/MLFromScratch/classical/linear_regression/cuda_backend/linear_regression.h
@@ -3,7 +3,7 @@
 
 #include <cuda_runtime.h>
 
-extern "C" {`
+extern "C" {
 void fit(const float *X, const float *Y, float *Beta, const int num_samples, const int num_input_features, const int num_output_features);
 void predict(const float *X, const float *Beta, float *Prediction, const int num_samples, const int num_input_features, const int num_output_features);
 float cost(const float *Y_pred, const float *Y, const int num_samples, const int num_output_features);
