fb066bd61cf3ad4d9fed9128d243fc55cb7885cc
Cole Foster <cmf290@miami.edu>
2025-02-17 13:58:30 -0500

    Reformatted init

--- src/MLFromScratch/classical/__init__.py
+++ src/MLFromScratch/classical/__init__.py
@@ -1,7 +1,21 @@
+from .supervised_model import SupervisedModel
+from .unsupervised_model import UnsupervisedModel
+
 from .linear_regression.py_interface.linear_regression_python import LinearRegressionPython
 from .linear_regression.py_interface.linear_regression_c import LinearRegressionC
 from .linear_regression.py_interface.linear_regression_cuda import LinearRegressionCUDA
+
 from .logistic_regression.py_interface.logistic_regression_python import LogisticRegressionPython
 from .logistic_regression.py_interface.logistic_regression_c import LogisticRegressionC
 from .logistic_regression.py_interface.logistic_regression_cuda import LogisticRegressionCUDA
-from .classical_model import ClassicalModel
+
+__all__ = [
+    SupervisedModel,
+    UnsupervisedModel,
+    LinearRegressionPython,
+    LinearRegressionC,
+    LinearRegressionCUDA,
+    LogisticRegressionPython,
+    LogisticRegressionC,
+    LogisticRegressionCUDA,
+]
