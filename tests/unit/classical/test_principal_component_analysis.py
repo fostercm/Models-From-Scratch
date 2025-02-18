d02ac62eb7a2e667b16a54e57677cf2aa89fad5e
Cole Foster <cmf290@miami.edu>
2025-02-17 23:52:48 -0500

    Added PCAC to tests

--- tests/unit/classical/test_principal_component_analysis.py
+++ tests/unit/classical/test_principal_component_analysis.py
@@ -1,6 +1,7 @@
 import unittest
 from MLFromScratch.classical import (
     PCAPython,
+    PCAC
 )
 from MLFromScratch.utils.python_utils.matrix_functions import standardize
 from sklearn.decomposition import PCA
@@ -12,51 +13,51 @@ np.random.seed(0)
 class TestPCA(unittest.TestCase):
     """
     Tests for the LogisticRegression class implementations in Python, C, and CUDA.
-    Ensures correctness for fitting, predicting, and calculating costs.
+    Ensures correctness for fitting, transforming, and calculating costs.
     """
 
-    def test_predict(self):
+    def test_transform(self):
         """
-        Test the predict method for all LogisticRegression implementations.
+        Test the transform method for all LogisticRegression implementations.
 
         Verifies the following:
-        - Ensures the model is fitted before making predictions.
+        - Ensures the model is fitted before making transformations.
         - Verifies that the input dimensions match the model's parameters.
-        - Checks that the predictions are made correctly based on a fitted model.
-        - Ensures that the predictions match expected values for a given input.
+        - Checks that the transformations are made correctly based on a fitted model.
+        - Ensures that the transformations match expected values for a given input.
         """
         X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]], dtype=np.float32)
         standard_X = standardize(X)
         sklearn_model = PCA()
         X_pred = sklearn_model.fit_transform(standard_X)
         
-        for model in [PCAPython()]:
+        for model in [PCAPython(), PCAC()]:
             
             # Test that the input is a numpy array
             with self.assertRaises(TypeError):
-                model.predict([1, 2, 3])
-                
-            predictions = model.predict(X)
+                model.transform([1, 2, 3])
+            
+            transformations = model.transform(X)
             
             for i in range(2):
                 try:
                     np.testing.assert_array_almost_equal(
-                        predictions[:,i], X_pred[:,i], decimal=4
+                        transformations[:,i], X_pred[:,i], decimal=4
                     )
                 except AssertionError:    
                     np.testing.assert_array_almost_equal(
-                        -predictions[:,i], X_pred[:,i], decimal=4
+                        -transformations[:,i], X_pred[:,i], decimal=4
                     )
             
-            # Check that the predictions are made correctly based on a fitted model
+            # Check that the transformations are made correctly based on a fitted model
             for i in range(1, 3):
-                predictions = model.predict(X, N_components=i)
+                transformations = model.transform(X, n_components=i)
                 np.testing.assert_array_almost_equal(
-                    model.predict(X,i), predictions[:,:i], decimal=4
+                    model.transform(X,i), transformations[:,:i], decimal=4
                 )
             
             np.testing.assert_array_almost_equal(
-                model.predict(X, explained_variance_ratio=0.85), predictions[:,:1], decimal=4
+                model.transform(X, explained_variance_ratio=0.85), transformations[:,:1], decimal=4
             )
                 
             
