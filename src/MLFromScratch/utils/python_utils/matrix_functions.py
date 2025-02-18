39b8a8cf0e839ffe7a46164d01570e8941e08b06
Cole Foster <cmf290@miami.edu>
2025-02-17 20:32:16 -0500

    Fixed normalization and standardization

--- src/MLFromScratch/utils/python_utils/matrix_functions.py
+++ src/MLFromScratch/utils/python_utils/matrix_functions.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def normalize(X):
     """Normalize the feature matrix X."""
-    return X - np.min(X, axis=1) / (np.max(X, axis=1) - np.min(X, axis=1))
+    return (X - np.min(X, axis=0)) / (np.max(X, axis=0) - np.min(X, axis=0))
 
 def standardize(X):
     """Standardize the feature matrix X."""
-    return (X - np.mean(X, axis=1)) / np.std(X, axis=1)
+    return (X - np.mean(X, axis=0)) / np.std(X, axis=0, ddof=1)
