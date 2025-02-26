ff4d4469d61c9ec6dce025ce392e33b55f6a9e5c
Cole Foster <cmf290@miami.edu>
2025-02-25 22:02:58 -0500

    Added KMeans to import

--- src/MLFromScratch/classical/__init__.py
+++ src/MLFromScratch/classical/__init__.py
@@ -2,10 +2,12 @@ from .linear_regression.py_interface.linear_regression import LinearRegression
 from .logistic_regression.py_interface.logistic_regression import LogisticRegression
 from .principal_component_analysis.py_interface.principal_component_analysis import PCA
 from .naive_bayes.py_interface.naive_bayes import NaiveBayes
+from .kmeans.py_interface.kmeans import KMeans
 
 __all__ = [
     LinearRegression,
     LogisticRegression,
     PCA,
-    NaiveBayes
+    NaiveBayes,
+    KMeans
 ]
