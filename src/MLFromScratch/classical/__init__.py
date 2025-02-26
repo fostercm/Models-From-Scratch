c5530d5cfbda59501d01b3a82d0b53dc4b94d53a
Cole Foster <cmf290@miami.edu>
2025-02-25 19:43:14 -0500

    Fix imports

--- src/MLFromScratch/classical/__init__.py
+++ src/MLFromScratch/classical/__init__.py
@@ -1,21 +1,12 @@
 from .linear_regression.py_interface.linear_regression import LinearRegression
 from .logistic_regression.py_interface.logistic_regression import LogisticRegression
-
-# from .logistic_regression.py_interface.logistic_regression_python import LogisticRegressionPython
-# from .logistic_regression.py_interface.logistic_regression_c import LogisticRegressionC
-# from .logistic_regression.py_interface.logistic_regression_cuda import LogisticRegressionCUDA
-
-from .principal_component_analysis.py_interface.principal_component_analysis_python import PCAPython
-from .principal_component_analysis.py_interface.principal_component_analysis_c import PCAC
-from .principal_component_analysis.py_interface.principal_component_analysis_cuda import PCACUDA
+from .principal_component_analysis.py_interface.principal_component_analysis import PCA
 
 from .naive_bayes.py_interface.naive_bayes_python import NaiveBayesPython
 
 __all__ = [
     LinearRegression,
     LogisticRegression,
-    PCAPython,
-    PCAC,
-    PCACUDA,
+    PCA,
     NaiveBayesPython
 ]
