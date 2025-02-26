37df0b130c1d094e5f9770c88a48d8507120956a
Cole Foster <cmf290@miami.edu>
2025-02-25 19:31:04 -0500

    Changed package structure

--- src/MLFromScratch/classical/__init__.py
+++ src/MLFromScratch/classical/__init__.py
@@ -1,28 +1,21 @@
-from .supervised_model import SupervisedModel
-from .unsupervised_model import UnsupervisedModel
+from .linear_regression.py_interface.linear_regression import LinearRegression
+from .logistic_regression.py_interface.logistic_regression import LogisticRegression
 
-from .linear_regression.py_interface.linear_regression_python import LinearRegressionPython
-from .linear_regression.py_interface.linear_regression_c import LinearRegressionC
-from .linear_regression.py_interface.linear_regression_cuda import LinearRegressionCUDA
-
-from .logistic_regression.py_interface.logistic_regression_python import LogisticRegressionPython
-from .logistic_regression.py_interface.logistic_regression_c import LogisticRegressionC
-from .logistic_regression.py_interface.logistic_regression_cuda import LogisticRegressionCUDA
+# from .logistic_regression.py_interface.logistic_regression_python import LogisticRegressionPython
+# from .logistic_regression.py_interface.logistic_regression_c import LogisticRegressionC
+# from .logistic_regression.py_interface.logistic_regression_cuda import LogisticRegressionCUDA
 
 from .principal_component_analysis.py_interface.principal_component_analysis_python import PCAPython
 from .principal_component_analysis.py_interface.principal_component_analysis_c import PCAC
 from .principal_component_analysis.py_interface.principal_component_analysis_cuda import PCACUDA
 
+from .naive_bayes.py_interface.naive_bayes_python import NaiveBayesPython
+
 __all__ = [
-    SupervisedModel,
-    UnsupervisedModel,
-    LinearRegressionPython,
-    LinearRegressionC,
-    LinearRegressionCUDA,
-    LogisticRegressionPython,
-    LogisticRegressionC,
-    LogisticRegressionCUDA,
+    LinearRegression,
+    LogisticRegression,
     PCAPython,
     PCAC,
-    PCACUDA
+    PCACUDA,
+    NaiveBayesPython
 ]
