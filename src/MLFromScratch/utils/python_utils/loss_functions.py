c3c1ee31b9f5386ab22423f28623987bbb1ef9b3
Cole Foster <cmf290@miami.edu>
2025-02-25 19:30:55 -0500

    Added language to loss functions

--- src/MLFromScratch/utils/python_utils/loss_functions.py
+++ src/MLFromScratch/utils/python_utils/loss_functions.py
@@ -1,7 +1,20 @@
 import numpy as np
+import ctypes
+import os
+
+# Load the C and CUDA shared libraries
+package_dir = os.path.dirname(os.path.abspath(__file__))
+
+clib_path = os.path.join(package_dir, "../../lib/libc_utils_shared.so")
+clib_path = os.path.normpath(clib_path)
+clib = ctypes.CDLL(clib_path)
+
+cudalib_path = os.path.join(package_dir, "../../lib/libcuda_utils_shared.so")
+cudalib_path = os.path.normpath(cudalib_path)
+cudalib = ctypes.CDLL(cudalib_path)
 
 
-def meanSquaredError(Y_pred: np.ndarray, Y_true: np.ndarray) -> float:
+def meanSquaredError(Y_pred: np.ndarray, Y_true: np.ndarray, language: str = 'python') -> float:
     """
     Compute the mean squared error between the predicted and true labels.
 
@@ -17,11 +30,16 @@ def meanSquaredError(Y_pred: np.ndarray, Y_true: np.ndarray) -> float:
     float
         Mean squared error.
     """
-    SCALE_FACTOR = 0.5 / Y_true.shape[0]
-    return SCALE_FACTOR * np.linalg.norm(Y_pred - Y_true) ** 2
+    if language == 'python':
+        return (0.5 / Y_true.shape[0]) * np.linalg.norm(Y_pred - Y_true) ** 2
+    elif language == 'c':
+        return clib.meanSquaredError(Y_pred, Y_true, Y_pred.shape[0], Y_pred.shape[1])
+    elif language == 'cuda':
+        return cudalib.meanSquaredError(Y_pred, Y_true, Y_pred.shape[0], Y_pred.shape[1])
+        
 
 
-def crossEntropy(Y_pred: np.ndarray, Y_true: np.ndarray) -> float:
+def crossEntropy(Y_pred: np.ndarray, Y_true: np.ndarray, language: str = 'python') -> float:
     """
     Compute the cross-entropy loss between the predicted and true labels.
 
@@ -37,13 +55,17 @@ def crossEntropy(Y_pred: np.ndarray, Y_true: np.ndarray) -> float:
     float
         Cross-entropy loss.
     """
-    SCALE_FACTOR = -1 / Y_true.shape[0]
+    if language == 'python':
+        SCALE_FACTOR = -1 / Y_true.shape[0]
 
-    if Y_pred.shape[1] == 1:
-        # Binary classification
-        return SCALE_FACTOR * np.sum(
-            Y_true * np.log(Y_pred + 1e-9) + (1 - Y_true) * np.log(1 - Y_pred)
-        )
-    else:
-        # Multi-class classification
-        return SCALE_FACTOR * np.sum(Y_true * np.log(Y_pred + 1e-9))
+        if Y_pred.shape[1] == 1:
+            # Binary classification
+            return SCALE_FACTOR * np.sum(Y_true * np.log(Y_pred + 1e-9) + (1 - Y_true) * np.log(1 - Y_pred))
+        else:
+            # Multi-class classification
+            return SCALE_FACTOR * np.sum(Y_true * np.log(Y_pred + 1e-9))
+    
+    elif language == 'c':
+        return clib.crossEntropy(Y_pred, Y_true, Y_pred.shape[0], Y_pred.shape[1])
+    elif language == 'cuda':
+        return cudalib.crossEntropy(Y_pred, Y_true, Y_pred.shape[0], Y_pred.shape[1])
