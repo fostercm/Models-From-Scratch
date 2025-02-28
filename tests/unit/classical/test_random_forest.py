08f72ea0de8b6db8e923fa6ef569fbcb77a69601
Cole Foster <cmf290@miami.edu>
2025-02-28 16:24:03 -0500

    Improved test

--- tests/unit/classical/test_random_forest.py
+++ tests/unit/classical/test_random_forest.py
@@ -11,11 +11,11 @@ class TestRandomForest(unittest.TestCase):
     def test(self):
 
         # Generate a synthetic classification dataset
-        X_cla, y_cla = make_classification(n_samples=500, n_features=3, n_classes=3, 
+        X_cla, y_cla = make_classification(n_samples=500, n_features=10, n_classes=3, 
                             n_clusters_per_class=1, n_redundant=0, 
                             n_informative=2, class_sep=6, random_state=42)
         
-        X_reg, Y_reg = make_regression(n_samples=500, n_features=3, n_targets=2, 
+        X_reg, Y_reg = make_regression(n_samples=500, n_features=10, n_targets=2, 
                        noise=0.1, random_state=42)
         
         for model in [
@@ -29,11 +29,11 @@ class TestRandomForest(unittest.TestCase):
             self.assertGreater(model.accuracy(y_pred,y_cla), 0.9)
         
         for model in [
-            RandomForest(language='Python', task='regression', n_trees=5, max_depth=5),
+            RandomForest(language='Python', task='regression', n_trees=3, max_depth=5),
         ]:
             # Fit the model to the data
             model.fit(X_reg, Y_reg)
             
             # Predict the target values and calculate the accuracy for the different distance types
             Y_pred = model.predict(X_reg)
-            self.assertGreater(model.RSquared(Y_pred,Y_reg), 0.8)
+            self.assertLess(model.RMSE(Y_pred,Y_reg),model.RMSE(Y_reg, np.mean(Y_reg, axis=0)[None, :]))
